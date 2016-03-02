8f2cea6a9f0f981322cf12fb96cf189676ddc18e
Raul Mur Artal <rmurartal@gmail.com>
2016-03-02 15:33:01 +0100

    Add include
    Some OpenCV versions seem to complain

--- src/ORBextractor.cc
+++ src/ORBextractor.cc
@@ -57,6 +57,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/features2d/features2d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <vector>
 
 #include "ORBextractor.h"
