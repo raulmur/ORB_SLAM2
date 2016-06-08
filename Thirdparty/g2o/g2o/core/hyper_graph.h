2015e945c2a6dd8694ab629c3ba2ecace0e85e64
julien.waechter <julien.waechter@gmail.com>
2016-06-08 09:20:07 +0200

    Updated g2o code: removed tr1

--- Thirdparty/g2o/g2o/core/hyper_graph.h
+++ Thirdparty/g2o/g2o/core/hyper_graph.h
@@ -34,13 +34,7 @@
 #include <vector>
 #include <limits>
 #include <cstddef>
-
-#ifdef _MSC_VER
 #include <unordered_map>
-#else
-#include <tr1/unordered_map>
-#endif
-
 
 /** @addtogroup graph */
 //@{
@@ -90,7 +84,7 @@ namespace g2o {
       typedef std::set<Edge*>                           EdgeSet;
       typedef std::set<Vertex*>                         VertexSet;
 
-      typedef std::tr1::unordered_map<int, Vertex*>     VertexIDMap;
+      typedef std::unordered_map<int, Vertex*>     VertexIDMap;
       typedef std::vector<Vertex*>                      VertexContainer;
 
       //! abstract Vertex, your types must derive from that one
