ffb16e66e573b5aa8315cd6ad751d45958323f92
Nicolas Degen <ndegen@me.com>
2017-06-19 22:45:47 +0200

    removed unused build steps

--- build.sh
+++ build.sh
@@ -1,31 +1,6 @@
-echo "Configuring and building Thirdparty/DBoW2 ..."
-
-cd Thirdparty/DBoW2
-rm -rf build/
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
-
-cd ../../g2o
-
-echo "Configuring and building Thirdparty/g2o ..."
-
-rm -rf build/
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make
-
-cd ../../../
-
-echo "Uncompress vocabulary ..."
-
 cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
 cd ..
-
-echo "Configuring and building ORB_SLAM2 ..."
 
 rm -rf build/
 mkdir build
