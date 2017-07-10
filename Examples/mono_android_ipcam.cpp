a7744f72379af4296426ecc517b585f369536e4c
Alkaid <zgf574564920@gmail.com>
2017-07-10 20:24:30 +0800

    rewrite mjpeg parser, used to be careless

--- Examples/mono_android_ipcam.cpp
+++ Examples/mono_android_ipcam.cpp
@@ -43,83 +43,104 @@ void register_handler()
 static size_t write_cb(char *buf, size_t n, size_t nmemb, void *p)
 {
     if (is_exit)
-	return 0;
+        return 0;
     size_t len = n*nmemb;
+LOOP:
     switch (stat) {
     case IDLE:
     {
-	if (!read_buf_isempty) {
-	    memcpy(read_buf + read_buf_len, buf, len);
-	    buf = read_buf;
-	    read_buf_isempty = true;
-	    len = read_buf_len + len;
-	    read_buf[len] = 0;
-	}
-	char *sub = strstr(buf, "--");
-	if (sub) {
-	    char *eol = strstr(sub, "\r");
-	    memcpy(signature, sub, eol-sub);
-	    signature[eol-sub] = 0;
-	    stat = WAIT_LENGTH;
-	}
-	else
-	    break;
+        if (!read_buf_isempty) {
+            memcpy(read_buf + read_buf_len, buf, len);
+            buf = read_buf;
+            read_buf_isempty = true;
+            len = read_buf_len + len;
+            read_buf[len] = 0;
+        }
+        received_length = 0;
+        char *sub = strstr(buf, "--");
+        if (sub) {
+            char *eol = strstr(sub, "\r");
+            if (!eol) {
+                goto WAIT_NEXT;
+            }
+            memcpy(signature, sub, eol-sub);
+            signature[eol-sub] = 0;
+            len -= eol - buf;
+            buf = eol;
+            stat = WAIT_LENGTH;
+        }
+        else goto WAIT_NEXT;
     }
     case WAIT_LENGTH:
     {
-	char *sub = strstr(buf, "Content-Length");
-	if (sub) {
-	    char *space = strstr(sub, " ");
-	    content_length = atoi(space);
-	    if (content_length > MAX_JPEG_BUF_LEN) {
-		fprintf(stderr, "Buffer overflow!! %d > %d\n", content_length, MAX_JPEG_BUF_LEN);
-		exit(-1);
-	    }
-	    char *pos_n = strstr(space, "\n");
-	    pos_n = strstr(pos_n+1, "\n");
-	    received_length = len-(pos_n-buf+1);
-	    memcpy(raw_jpeg_buf, pos_n+1, received_length);
-	    stat = RECV_DATA;
-	}
-	break;
+        if (!read_buf_isempty) {
+            memcpy(read_buf + read_buf_len, buf, len);
+            buf = read_buf;
+            read_buf_isempty = true;
+            len = read_buf_len + len;
+            read_buf[len] = 0;
+        }
+        char *sub = strstr(buf, "Content-Length");
+        if (sub) {
+            char *space = strstr(sub, " ");
+            if (!space) goto WAIT_NEXT;
+            content_length = atoi(space);
+            if (content_length > MAX_JPEG_BUF_LEN) {
+                fprintf(stderr, "Buffer overflow!! %d > %d\n", content_length, MAX_JPEG_BUF_LEN);
+                exit(-1);
+            }
+            char *pos_n = strstr(space, "\n");
+            if (!pos_n) goto WAIT_NEXT;
+            pos_n = strstr(pos_n+1, "\n");
+            if (!pos_n) goto WAIT_NEXT;
+            stat = RECV_DATA;
+            len -= pos_n+1 - buf;
+            if (len > 0)
+                buf = pos_n+1;
+            else
+                break;
+        }
+        else goto WAIT_NEXT;
     }
     case RECV_DATA:
-	if (received_length + len < content_length) {
-	    memcpy(raw_jpeg_buf+received_length, buf, len);
-	    received_length += len;
-	    break;
-	}
-	else {
-	    memcpy(raw_jpeg_buf+received_length, buf, content_length - received_length);
-	    cv::Mat raw_data(1, content_length, CV_8UC1, raw_jpeg_buf);
-	    cv::Mat img = cv::imdecode(raw_data, CV_LOAD_IMAGE_COLOR);
+        if (received_length + len < content_length) {
+            memcpy(raw_jpeg_buf+received_length, buf, len);
+            received_length += len;
+            break;
+        }
+        else {
+            memcpy(raw_jpeg_buf+received_length, buf, content_length - received_length);
+            cv::Mat raw_data(1, content_length, CV_8UC1, raw_jpeg_buf);
+            cv::Mat img = cv::imdecode(raw_data, CV_LOAD_IMAGE_COLOR);
 
-	    // deal with a frame of jpeg
-	    ORB_SLAM2::System *mpSLAM = (ORB_SLAM2::System *)p;
-	    struct timeval timestamp;
-	    gettimeofday(&timestamp, NULL);
-	    mpSLAM->TrackMonocular(img, timeval2double(timestamp));
-	    // prepare for next frame
-	    memcpy(read_buf, buf+content_length-received_length, len-(content_length-received_length));
-	    read_buf_isempty = false;
-	    read_buf_len = len-(content_length-received_length);
-	    stat = IDLE;
-	    read_buf[read_buf_len]=0;
-	    break;
-	}
+            // deal with a frame of jpeg
+            ORB_SLAM2::System *mpSLAM = (ORB_SLAM2::System *)p;
+            struct timeval timestamp;
+            gettimeofday(&timestamp, NULL);
+            mpSLAM->TrackMonocular(img, timeval2double(timestamp));
+            stat = IDLE;
+            buf += content_length - received_length;
+            len -= content_length - received_length;
+            goto LOOP;
+            break;
+        }
     default:
-	fprintf(stderr, "Unknown Status\n");
-	exit(-1);
+        fprintf(stderr, "Unknown Status\n");
+        exit(-1);
     }
-
+    return n*nmemb;
+WAIT_NEXT:
+    memcpy(read_buf, buf, len);
+    read_buf_isempty = false;
+    read_buf_len = len;
     return n*nmemb;
 }
 int main(int argc, char **argv) {
     register_handler();
     if(argc != 4)
     {
-	cerr << endl << "Usage: mono_http_stream path_to_vocabulary path_to_settings http://mjpeg/stream/address" << endl;
-	return 1;
+        cerr << endl << "Usage: mono_http_stream path_to_vocabulary path_to_settings http://mjpeg/stream/address" << endl;
+        return 1;
     }
     stat = IDLE;
     ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
@@ -137,7 +158,7 @@ int main(int argc, char **argv) {
     char c;
     do {
         fscanf(stdin, "%c", &c);
-     } while  (c == 'y');
+    } while  (c == 'y');
     SLAM.Shutdown();
     SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
     return 0;
