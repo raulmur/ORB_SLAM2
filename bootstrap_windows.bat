f49a896d3bbb918e49bf00c9295f846b0cb09716
Ansuman.Pattnaik <Ansuman.Pattnaik@haman.com>
2017-09-08 14:47:04 +0530

    fix bootstrap_windows.bat issue

--- bootstrap_windows.bat
+++ bootstrap_windows.bat
@@ -10,19 +10,16 @@ if NOT "%~1"=="" set "OrbSlamPlatform=%~1"
 if NOT "%~2"=="" set "OrbSlamToolset=%~2"
 if NOT "%~3"=="" set "Buildtype=%~3"
 
-set "VcPkgGitUrl=https://github.com/paul-michalik/vcpkg.git"
 set "VcPkgTag=0.0.81-5"
 set "VcPkgDir=%~d0\Software\vcpkg\vcpkg-%VcPkgTag%"
 if defined VCPKG_ROOT_DIR if /i not "%VCPKG_ROOT_DIR%"=="" set "VcPkgDir=%VCPKG_ROOT_DIR%"
 set "VcPkgTriplet=%OrbSlamPlatform%-windows"
 if defined VCPKG_DEFAULT_TRIPLET if /i not "%VCPKG_DEFAULT_TRIPLET%"=="" set "VcpkgTriplet=%VCPKG_DEFAULT_TRIPLET%"
 set "VcPkgPath=%VcPkgDir%\vcpkg.exe"
-set "ORB_3RD_PARTY_LIB_DIR=%VcPkgDir%\installed\%VcPkgTriplet%"
+set "ORB_SLAM_3RD_PARTY_LIB_DIR=%VcPkgDir%\installed\%VcPkgTriplet%"
 
-echo. & echo Installing/updating vcpkg...
-call :BootstrapVcPkg "%VcPkgGitUrl%" "%VcPkgTag%" "%VcPkgDir%"
-if %errorlevel% neq 0 echo An error occured in %~n0, bailing out & exit /b %errorlevel%
-echo. & echo vcpkg %MacsVcPkgTag% successfully installed...
+
+if NOT EXIST "%VcPkgDir%" echo "vcpkg path is not set correctly", bailing out & exit /b 0
 
 rem ==============================
 rem Update and Install packages.
@@ -32,49 +29,3 @@ call "%VcPkgPath%" install opencv pangolin --triplet %VcPkgTriplet%
 
 rem build project
 call "%~dp0build_windows.bat" %*
-rem ==============================
-rem Utilities. Do not add anything beyond this point.
-rem ==============================
-
-goto :eof
-
-:BootstrapVcPkg
-setlocal
-    set "LocErrorState=0"
-    set "LocVcPkgGit=%~1"
-    set "LocVcPkgTag=%~2"
-    set "LocVcPkgDir=%~3"
-    set "LocVcPkgTag=tags/%LocVcPkgTag%"
-
-    echo Checking for existence of "%LocVcPkgDir%"...
-    if not exist "%LocVcPkgDir%" (
-        echo Attempt to: git clone "%LocVcPkgGit%" "%LocVcPkgDir%"...
-        git clone "%LocVcPkgGit%" "%LocVcPkgDir%"
-    )
-    if %errorlevel% neq 0 echo An error occured in %~n0, bailing out & exit /b %errorlevel%
-
-    pushd "%LocVcPkgDir%"
-    if %errorlevel% neq 0 echo An error occured in %~n0, bailing out & exit /b %errorlevel%
-
-    echo Attempt to: git fetch --all --tags --prune...
-    git fetch --all --tags --prune
-    if %errorlevel% neq 0 echo An error occured in %~n0, bailing out & exit /b %errorlevel
-
-    rem echo Attempt to: git checkout %LocVcPkgTag%...
-    rem git checkout %LocVcPkgTag%
-    rem if %errorlevel% neq 0 echo An error occured in %~n0, bailing out & exit /b %errorlevel
-
-	echo Attempt to: git checkout bug/pangolin...
-    git checkout bug/pangolin
-    if %errorlevel% neq 0 echo An error occured in %~n0, bailing out & exit /b %errorlevel
-	
-    rem 
-    rem It is not clear to me when to call bootstrapping script, so I do call it always...
-    rem 
-    if not exist vcpkg.exe call "bootstrap-vcpkg.bat"
-    if %errorlevel% neq 0 echo An error occured in %~n0, bailing out & exit /b %errorlevel%
-
-    popd
-endlocal & exit /b %errorlevel%
-
-goto :eof
--- build_windows.bat
+++ build_windows.bat
@@ -23,18 +23,19 @@ if "%OrbSlamPlatform%"=="x64" (
 )
 
 rem set ORB_3RD_PARTY_LIB_DIR for opencv and pangolin library path
-if "%ORB_3RD_PARTY_LIB_DIR%"=="" (
-echo "it is not defined"
-set "ORB_3RD_PARTY_LIB_DIR=%USERPROFILE%\Software\lib\%OrbSlamPlatform%-windows-%OrbSlamToolset%"
+if "%ORB_SLAM_3RD_PARTY_LIB_DIR%"=="" (
+set "ORB_SLAM_3RD_PARTY_LIB_DIR=%USERPROFILE%\Software\lib\%OrbSlamPlatform%-windows-%OrbSlamToolset%"
 )
 
-set "CMAKE_PREFIX_PATH=%ORB_3RD_PARTY_LIB_DIR%;%CMAKE_PREFIX_PATH%"
+if NOT EXIST "%ORB_SLAM_3RD_PARTY_LIB_DIR%" echo "OrbSlam dependant lib path is not set correctly", bailing out & exit /b 0
+
+set "CMAKE_PREFIX_PATH=%ORB_SLAM_3RD_PARTY_LIB_DIR%;%CMAKE_PREFIX_PATH%"
 
 rem creating and build orb-slam project
 set "BuildDir=%~dp0\products\windows-%OrbSlamPlatform%-%OrbSlamToolset%"-%Buildtype%
 if not exist "%BuildDir%" mkdir %BuildDir%
 cd "%BuildDir%"
 
-call cmake.exe -G "%OrbSlamCMakeGeneratorName%" -DORBSLAM2_STATIC_LIB=ON -DG2O_STATIC_LIB=ON -DDBOW2_STATIC_LIB=ON -DBUILD_EXAMPLES=ON "%~dp0"
+call cmake.exe -G "%OrbSlamCMakeGeneratorName%" -DORBSLAM2_STATIC_LIB=ON -DG2O_STATIC_LIB=ON -DDBOW2_STATIC_LIB=ON -DBUILD_EXAMPLES=ON -DBUILD_THIRDPARTY_LIB=ON "%~dp0"
 call cmake.exe --build . --config %Buildtype%
 endlocal
