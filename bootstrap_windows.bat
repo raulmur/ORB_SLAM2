667346f1569c33771a28e393937a0a88dba56819
Ansuman.Pattnaik <Ansuman.Pattnaik@haman.com>
2017-09-11 15:00:12 +0530

    separate bootstrap_windows.bat from build_windows.bat

--- bootstrap_windows.bat
+++ bootstrap_windows.bat
@@ -16,7 +16,6 @@ if defined VCPKG_ROOT_DIR if /i not "%VCPKG_ROOT_DIR%"=="" set "VcPkgDir=%VCPKG_
 set "VcPkgTriplet=%OrbSlamPlatform%-windows"
 if defined VCPKG_DEFAULT_TRIPLET if /i not "%VCPKG_DEFAULT_TRIPLET%"=="" set "VcpkgTriplet=%VCPKG_DEFAULT_TRIPLET%"
 set "VcPkgPath=%VcPkgDir%\vcpkg.exe"
-set "ORB_SLAM_3RD_PARTY_LIB_DIR=%VcPkgDir%\installed\%VcPkgTriplet%"
 
 
 if NOT EXIST "%VcPkgDir%" echo "vcpkg path is not set correctly", bailing out & exit /b 0
@@ -28,4 +27,30 @@ call "%VcPkgPath%" update
 call "%VcPkgPath%" install opencv pangolin --triplet %VcPkgTriplet%
 
 rem build project
-call "%~dp0build_windows.bat" %*
+
+set "OrbSlamCMakeGeneratorName=Visual Studio 14 2015"
+
+if "%OrbSlamPlatform%"=="x86" (
+    if "%OrbSlamToolset%"=="v140" set "OrbSlamCMakeGeneratorName=Visual Studio 14 2015"
+    if "%OrbSlamToolset%"=="v141" set "OrbSlamCMakeGeneratorName=Visual Studio 15 2017"
+)
+
+if "%OrbSlamPlatform%"=="x64" (
+    if "%OrbSlamToolset%"=="v140" set "OrbSlamCMakeGeneratorName=Visual Studio 14 2015 Win64"
+    if "%OrbSlamToolset%"=="v141" set "OrbSlamCMakeGeneratorName=Visual Studio 15 2017 Win64"
+)
+
+rem set ORB_3RD_PARTY_LIB_DIR for opencv and pangolin library path
+set "ORB_SLAM_3RD_PARTY_LIB_DIR=%VcPkgDir%\installed\%VcPkgTriplet%"
+
+if NOT EXIST "%ORB_SLAM_3RD_PARTY_LIB_DIR%" echo "OrbSlam dependant lib path is not set correctly", bailing out & exit /b 0
+
+set "CMAKE_PREFIX_PATH=%ORB_SLAM_3RD_PARTY_LIB_DIR%;%CMAKE_PREFIX_PATH%"
+
+rem creating and build orb-slam project
+set "BuildDir=%~dp0\products\windows-%OrbSlamPlatform%-%OrbSlamToolset%"-%Buildtype%
+if not exist "%BuildDir%" mkdir %BuildDir%
+cd "%BuildDir%"
+
+call cmake.exe -G "%OrbSlamCMakeGeneratorName%" -DORBSLAM2_STATIC_LIB=ON -DG2O_STATIC_LIB=ON -DDBOW2_STATIC_LIB=ON -DBUILD_EXAMPLES=ON -DBUILD_THIRDPARTY_LIB=ON "%~dp0"
+call cmake.exe --build . --config %Buildtype%
