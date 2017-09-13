e8c25969144f1ef95a1d3980f76d1f8bd1544bb7
Ansuman.Pattnaik <Ansuman.Pattnaik@haman.com>
2017-09-13 20:42:52 +0530

    building Orb-slam2 from ubuntu

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y \
-    build-essential g++ autotools-dev cmake git doxygen \
-    python-dev \
-    libglew-dev \
-    ffmpeg \
-    libavcodec-dev \
-    libavutil-dev \
-    libavformat-dev \
-    libswscale-dev \
-    libdc1394-22-dev \
-    libraw1394-dev \
-    libjpeg-dev \
-    libpng12-dev \
-    libtiff5-dev \
-    libopenexr-dev \
-    libeigen3-dev \
-    libopencv-dev
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y autoremove
+
+apt-get install -y \
+	build-essential \
+	cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev \
+	python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev \
+	libeigen3-dev
+
+git clone https://github.com/opencv/opencv.git
+cd opencv
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..
+make -j $(($(nproc) + 1 ))
+make install
+ldconfig
+cd ../..
 
 git clone https://github.com/stevenlovegrove/Pangolin.git
 cd Pangolin
-mkdir build
+mkdir -p build
 cd build
 cmake ..
 cmake --build .
