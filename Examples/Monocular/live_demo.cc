ae12718566fe64983d1c8d93240adbaa475d0307
Roshnee <roshneekishore@yahoo.in>
2017-11-15 11:09:16 +0100

    Code for live demo

--- Examples/Monocular/live_demo.cc
+++ Examples/Monocular/live_demo.cc
@@ -8,27 +8,52 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
 
-VideoCapture cap(0);
-if(!cap.isOpened())
+#ifdef COMPILEDWITHC11
+#define SET_CLOCK(t0)
+  std::chrono::steady_clock::time_point t0 = std::chrono::steady_clock::now();
+
+#else
+
+#define SET_CLOCK(t0)
+  std::chrono::monotonic_clock::time_point t0 = std::chrono::monotonic_clock::now();
+
+#define TIME_DIFF(t0, t1)
+  std::chrono::duration_cast<std::chrono::duration<double>>(t1-t0).count();
+
+int main(int argc, char **argv[])
 {
-  cout<<"Video not available";
-}
-else
-{
-  Mat frame;
-  while(true){
+  //Check if the command-line arguments are given correctly
+  if (!argc=4)
+  {  
+    cerr<<"required path to vocabulary;path to settings;number of seconds";
+  }
+
+  //Capture video from webcam
+  VideoCapture cap(0);
+  if(!cap.isOpened())
+  {
+    //Throw an error message if there is no video stream
+    cout<<"Video not available.. Video stream not available";
+  }
+  //initializing threads for the slam process
+  ORB_SLAM2::System SLAM(argv[1],argv[2],ORBSLAM2::System::MONOCULAR,true); // argv[1] is the vocabulary file and argv[2] is the settings file 
+  cv::Mat frame;
+  timetorun=atof(argv[3]);
+  SET_CLOCK(t0); 
+  while(true)
+  {
      cap.grab();
      cap>>frame;
-
-//Creating a SLAM system 
-//ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
-
-//cout <<"Start processing sequence..."<<endl;
-//cout<<"Reading input images from the video stream..."<<endl;
-//cout<<"Reading input frame by frame..."<<endl;
+     SET_CLOCK(t1);
+     tframe=TIME_DIFF(t0,t1);
+     if(tframe>timetorun)
+     {
+       break;
+     }
+     cout <<"Start processing sequence..."<<endl;
+     cout<<"Reading input images from the video stream..."<<endl;
+     cout<<"Reading input frame by frame..."<<endl;
 
 //if(frame.empty())
 //{
