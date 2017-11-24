bc53d8f8d783763dad3c69c8bab7db440492d89f
Paul Michalik <paul.michalik@outlook.com>
2017-11-24 08:21:59 +0100

    Remove sudo

--- docker/build-from-docker.sh
+++ docker/build-from-docker.sh
@@ -4,7 +4,7 @@ CurDir=$(dirname $0)
 ProjectDir=${CurDir}/..
 cd ${ProjectDir}
 
-sudo docker run --rm -it --volume="$(pwd):/root/orb-slam2" \
+docker run --rm -it --volume="$(pwd):/root/orb-slam2" \
     shanmukhananda/orb-slam2:latest \
     /bin/bash -c \
     "/root/orb-slam2/scripts/linux/build.sh"
