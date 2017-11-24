87b2064e2c6a23db7b5d8feb4c9f62e721d498ce
Paul Michalik <paul.michalik@outlook.com>
2017-11-24 08:20:25 +0100

    Remove sudo from script

--- docker/run-from-docker.sh
+++ docker/run-from-docker.sh
@@ -10,8 +10,8 @@ then
     exit 1
 fi
 
-sudo xhost +local:root
-sudo docker run --rm -it \
+xhost +local:root
+docker run --rm -it \
     --net=host \
     --ipc=host \
     --device=/dev/dri:/dev/dri \
