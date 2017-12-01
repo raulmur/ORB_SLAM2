7c5173c5274f635460b8adafccde89b2e51fb8d3
a-jahani <a.jahaniamiri@gmail.com>
2017-12-01 14:09:49 -0700

    set viewer to false

--- Examples/RGB-D/rgbd_tum.cc
+++ Examples/RGB-D/rgbd_tum.cc
@@ -62,7 +62,7 @@ int main(int argc, char **argv)
     }
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::RGBD,true);
+    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::RGBD,false);
 
     // Vector for tracking time statistics
     vector<float> vTimesTrack;
