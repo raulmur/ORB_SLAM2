bb3c73aca4273bacba3de85e1b06867ddaf589ea
rpfly3 <rpfly0818@gmail.com>
2018-08-20 22:47:57 -0400

    solve loading/saving large map

--- src/FrameDrawer.cc
+++ src/FrameDrawer.cc
@@ -29,9 +29,12 @@
 namespace ORB_SLAM2
 {
 
-FrameDrawer::FrameDrawer(Map* pMap):mpMap(pMap)
+FrameDrawer::FrameDrawer(Map* pMap, bool bReuseMap):mpMap(pMap)
 {
-    mState=Tracking::SYSTEM_NOT_READY;
+    if (bReuseMap)
+        mState=Tracking::LOST;
+    else
+        mState=Tracking::SYSTEM_NOT_READY;
     mIm = cv::Mat(480,640,CV_8UC3, cv::Scalar(0,0,0));
 }
 
