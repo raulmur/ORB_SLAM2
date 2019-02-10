01003e7aa8f4ac409d95c58cc89c3b79567c1f76
Toraudonn <toraudonn@gmail.com>
2019-02-10 18:17:48 +0900

    add some files

--- build.sh
+++ build.sh
@@ -27,5 +27,5 @@ echo "Configuring and building ORB_SLAM2 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.local
 make -j
