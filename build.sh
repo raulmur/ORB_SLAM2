55e9422ec6dd4fcdf59e1ee507ce2d584508a547
apoorvkh <apoorv.khand@gmail.com>
2020-12-06 23:08:15 +0000

    Binary ORBvoc by default

--- build.sh
+++ build.sh
@@ -17,12 +17,6 @@ make -j
 
 cd ../../../
 
-echo "Uncompress vocabulary ..."
-
-cd Vocabulary
-tar -xf ORBvoc.txt.tar.gz
-cd ..
-
 echo "Configuring and building ORB_SLAM2 ..."
 
 mkdir build
@@ -30,6 +24,3 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 cd ..
-
-echo "Converting vocabulary to binary"
-./tools/bin_vocabulary
--- tools/bin_vocabulary.cc
+++ tools/bin_vocabulary.cc
@@ -45,8 +45,8 @@ int main(int argc, char **argv) {
   cout << "BoW load/save benchmark" << endl;
   ORB_SLAM2::ORBVocabulary* voc = new ORB_SLAM2::ORBVocabulary();
 
-  load_as_text(voc, "Vocabulary/ORBvoc.txt");
-  save_as_binary(voc, "Vocabulary/ORBvoc.bin");
+  load_as_binary(voc, "ORBvoc.bin");
+  save_as_text(voc, "ORBvoc.txt");
 
   return 0;
 }
